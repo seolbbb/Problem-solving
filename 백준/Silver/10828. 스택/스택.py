e451dc40e1f741182f601b76dac6a5036472d5f2
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2024-12-15 23:48:12 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,29 @@
+import sys
+stack = []
+
+n = int(input())
+
+for i in range(n):
+    
+    com = list(sys.stdin.readline().split())
+    size = len(stack)
+
+    if len(com) == 2:
+        stack.append(com[1])
+    elif com[0] == 'pop':
+        if size != 0:    
+            print(stack.pop())
+        else:
+            print(-1)
+    elif com[0] == 'size':
+        print(size)
+    elif com[0] == 'empty':
+        if size != 0:
+            print(0)
+        else:
+            print(1)
+    elif com[0] == 'top':
+        if size != 0:    
+            print(stack[-1])
+        else:
+            print(-1)
