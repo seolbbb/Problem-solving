d59df44ca56770a77ec319cfe7db439d54cd1b23
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2024-12-17 12:20:21 +0900

    [Bronze II] Title: 방 배정, Time: 32 ms, Memory: 32412 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,23 @@
+import sys
+
+n,k = map(int,sys.stdin.readline().split())
+dic = {}
+cnt = 0
+for i in range(1,7):
+    for j in range(2):
+        dic[(j,i)] = 0
+
+for i in range(n):
+    s, y = map(int,sys.stdin.readline().split())
+    dic[(s,y)] += 1
+
+for i in range(1,7):
+    for j in range(2):
+        if dic[(j,i)] == 0:
+            continue
+        if 1 <= dic[(j,i)] <= k:
+            cnt += 1
+        else:
+            cnt += (dic[(j,i)] // k) + 1
+            
+print(cnt)
