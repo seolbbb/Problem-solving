41fb772df79b9aec73489083ee6265e88d25de9f
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2024-12-19 04:05:57 +0900

    [Silver I] Title: 1로 만들기 2, Time: 672 ms, Memory: 80656 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,30 +1,26 @@
 n = int(input())
-dp = [[0, []] for _ in range(1000001)]
-dp[1][0] = 0
-dp[1][1] = [1]
-dp[2][0] = 1
-dp[2][1] = [1,2]
-dp[3][0] = 1
-dp[3][1] = [1,3]
+dp = [0 for _ in range(n+1)]
+pre = [0 for _ in range(n+1)]
 
-for i in range(4,n+1):
-    tmp = []
-    num = 0
-    a = float('inf')
-    b = float('inf')
-    c = dp[i-1][0] + 1
-    tmp.append((c,i-1))
+dp[1] = 0
+pre[1] = 0
 
-    if i % 3 == 0:
-        a = dp[i//3][0] + 1
-        tmp.append((a,i//3))
-    if i % 2 == 0:
-        b = dp[i//2][0] + 1
-        tmp.append((b,i//2))
-    mint, num = min(tmp)
+for i in range(2,n+1):
+    dp[i] = dp[i-1] + 1
+    pre[i] = i-1
 
-    dp[i][0] = mint
-    dp[i][1] = dp[num][1] + [i]
+    if i % 3 == 0 and dp[i] > dp[i//3] + 1:
+        dp[i] = dp[i//3] + 1
+        pre[i] = i//3
+    if i % 2 == 0 and dp[i] > dp[i//2] + 1:
+        dp[i] = dp[i//2] + 1
+        pre[i] = i//2
 
-print(dp[n][0])
-print(*(dp[n][1][::-1]))
+print(dp[n])
+
+cur = n
+while True:
+    print(cur, end = ' ')
+    if cur == 1:
+        break
+    cur = pre[cur]
