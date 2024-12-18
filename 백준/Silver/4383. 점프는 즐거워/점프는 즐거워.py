4dec096e48f55c8a21f704e1f94879a9ee1c146d
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2024-12-18 16:12:52 +0900

    [Silver V] Title: 점프는 즐거워, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/4383. 점프는 즐거워/점프는 즐거워.py
+++ 백준/Silver/4383. 점프는 즐거워/점프는 즐거워.py
@@ -3,17 +3,16 @@ import sys
 while True:
     try:
         lst = list(map(int,sys.stdin.readline().split()))
-        s = [i for i in range(1,lst[0])]
-        o = 0
-        for i in range(1,lst[0]):
-            if abs(lst[i]-lst[i+1]) in s:
-                s.remove(abs(lst[i]-lst[i+1]))
-                pass
-            else:
-                print('Not jolly')
-                o = 1
-                break
-        if o == 0:
+        if lst[0] == 1:
             print('Jolly')
+            continue
+        s = [i for i in range(1,lst[0])]
+        b = []
+        for i in range(1,lst[0]):
+            b.append(abs(lst[i]-lst[i+1]))
+        if sorted(b) == s:
+            print('Jolly')
+        else:
+            print('Not jolly')
     except:
         break
