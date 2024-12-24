1ebceeed347c1326bf4795556bd7287a4d06167e
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2024-12-24 19:01:17 +0900

    [Gold V] Title: 퇴사 2, Time: 3120 ms, Memory: 348768 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -3,14 +3,15 @@ input = sys.stdin.readline
 
 n = int(input())
 dp = [0 for _ in range(n+1)]
-tp = [[0,0]]
+tp = [[]for _ in range(n+1)]
 
 for i in range(n):
-    tp.append(list(map(int, input().split())))
+    tp[i] = list(map(int, input().split()))
 
-for i in range(1, n + 1):
-    if i + tp[i][0] <= n + 1:
-        dp[i + tp[i][0] - 1] = max(dp[i + tp[i][0] - 1], dp[i - 1] + tp[i][1])
-    dp[i] = max(dp[i], dp[i - 1])
+for i in range(n):
+    t, p = tp[i]
+    if i + t <= n:
+        dp[i + t] = max(dp[i + t], dp[i] + p)
+    dp[i + 1] = max(dp[i + 1], dp[i])
 
 print(max(dp))
