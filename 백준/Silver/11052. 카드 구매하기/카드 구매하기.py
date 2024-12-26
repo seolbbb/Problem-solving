92cf4a02322bff27a560f2bf645a5fe264b54a5a
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2024-12-26 11:15:15 +0900

    [Silver I] Title: 카드 구매하기, Time: 100 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -5,7 +5,7 @@ n = int(input())
 dp = [0] + list(map(int, input().split()))
 
 for i in range(1, n+1):
-    for j in range(i):
+    for j in range(1,(i//2)+1):
         dp[i] = max(dp[i], dp[j] + dp[i-j])
 
 print(max(dp))
