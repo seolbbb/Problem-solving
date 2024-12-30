789e481072aac568558b4fba83c5d20e1522c101
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2024-12-31 00:51:18 +0900

    [Gold IV] Title: 부분합, Time: 128 ms, Memory: 42168 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n,s = map(int,input().split())
+seq = list(map(int,input().split()))
+head = 0
+tail = 0
+partial = seq[0]
+length = 100001
+cnt = 1
+
+while True:
+    if partial >= s and head == tail:
+        length = 1
+        break
+    while partial < s and head < n-1:
+        head += 1
+        partial += seq[head]
+        cnt += 1
+    while partial >= s:
+        length = min(length,cnt)
+        if head > tail:
+            partial -= seq[tail]
+            tail += 1
+            cnt -= 1
+        else:
+            break
+    if head == n-1 or length == 1:
+        break
+
+if length == 100001:
+    print(0)
+else:
+    print(length)
