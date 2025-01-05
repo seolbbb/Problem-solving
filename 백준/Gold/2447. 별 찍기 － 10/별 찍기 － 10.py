6f477ca0ccf73ddccdc11688d34466068331aff7
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-01-05 21:03:40 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 256 ms, Memory: 81200 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -13,9 +13,26 @@ def star(r,c,num):
                 continue
             star(r+i*(num//3),c+j*(num//3),num//3)
 
+def star2(num):
+    if num == 1:
+        return ['*']
+    
+    stars = star2(num//3)
+    l = []
+
+    for s in stars:
+        l.append(s*3)
+    for s in stars:
+        l.append(s + ' ' * (num//3) + s)
+    for s in stars:
+        l.append(s*3)
+    return l
+
 n = int(input())
 arr = [[' ' for _ in range(n)] for _ in range(n)]
-star(0,0,n)
 
-for i in range(n):
-    print(''.join(arr[i]))
+print('\n'.join(star2(n)))
+
+# star(0,0,n)
+# for i in range(n):
+#     print(''.join(arr[i]))
