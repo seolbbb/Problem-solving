7de3a895ae872f8791e5fccc132f50eec0feb58c
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-01-14 15:18:45 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 3512 ms, Memory: 42784 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+n, m = map(int,input().split())
+
+dist = [-1 for _ in range(101)]
+skip = [0 for _ in range(101)]
+queue = deque()
+
+for _ in range(n+m):
+    s, d = map(int,input().split())
+    skip[s] = d
+
+queue.append(1)
+dist[1] = 0
+
+while queue:
+    x = queue.popleft()
+    for i in range(1,7):
+        nx = x + i
+        if nx > 100:
+            continue
+        if skip[nx] > 0:
+            nx = skip[nx]
+        if dist[nx] >= 0 and dist[nx] < dist[x] + 1:
+            continue
+        dist[nx] = dist[x] + 1
+        queue.append(nx)
+
+print(dist[100])
