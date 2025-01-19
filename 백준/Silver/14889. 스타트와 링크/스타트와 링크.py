a98aad90f1d96d31342993be6ae94c2930a36dcf
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-01-19 15:31:01 +0900

    [Silver I] Title: 스타트와 링크, Time: 2928 ms, Memory: 39116 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -7,14 +7,14 @@ def backtrack(start):
         team1_score = 0
         team2_score = 0
 
-        for i in team1:
-            for j in team1:
-                if i != j:
-                    team1_score += s[i-1][j-1]
-        for i in team2:
-            for j in team2:
-                if i != j:
-                    team2_score += s[i-1][j-1]
+        for i in range(len(team1)):
+            for j in range(i+1,len(team1)):
+                a, b = team1[i], team1[j]
+                team1_score += s[a-1][b-1] + s[b-1][a-1]
+        for i in range(len(team2)):
+            for j in range(i+1,len(team2)):
+                a, b = team2[i], team2[j]
+                team2_score += s[a-1][b-1] + s[b-1][a-1]
         
         results.append(abs(team2_score - team1_score))
         return
