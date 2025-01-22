5730af64d648efa963255c5ef6934014904e7b4f
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-01-23 01:08:25 +0900

    [Gold V] Title: 치킨 배달, Time: 460 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,37 +1,9 @@
 import sys
-import copy
 from itertools import combinations
-from collections import deque
 input = sys.stdin.readline
-
-# def bfs(row, col, n_arr):
-#     dist = [[-1 for _ in range(n)] for _ in range(n)]
-#     queue = deque()
-#     queue.append((row,col))
-#     dist[row][col] = 0
-
-#     while queue:
-#         x,y = queue.popleft()
-#         for i in range(4):
-#             nx = x + dx[i]
-#             ny = y + dy[i]
-
-#             if nx < 0 or nx >= n or ny < 0 or ny >= n:
-#                 continue
-#             if dist[nx][ny] >= 0:
-#                 continue
-#             if n_arr[nx][ny] == 2:
-#                 return dist[x][y] + 1
-            
-#             queue.append((nx,ny))
-#             dist[nx][ny] = dist[x][y] + 1
-    
-# chicken 13 C 6 = 1716, bfs 2500 * 100
 
 n, m = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(n)]
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
 chicken = []
 home = []
 ans = float('inf')
@@ -53,6 +25,7 @@ for i in range(len(cases)):
         for r2, c2 in cases[i]:
             temp = min(temp, abs(r1-r2) + abs(c1-c2))
         dist += temp
+        
     ans = min(ans, dist)
 
 print(ans)
