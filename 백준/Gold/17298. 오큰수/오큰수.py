0d07760d7590c7221ed4c9b8610ac34eb23c0f01
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-01-23 22:08:43 +0900

    [Gold IV] Title: 오큰수, Time: 1092 ms, Memory: 137300 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,17 +1,15 @@
 import sys
-import heapq
 input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int,input().split()))
 ans = [-1 for _ in range(n)]
-hq = []
+stack = []
 
 for i in range(n):
-    heapq.heappush(hq, (arr[i],i))
-
-    while hq and hq[0][0] < arr[i]:
-        num, idx = heapq.heappop(hq)
+    while stack and arr[stack[-1]] < arr[i]:
+        idx = stack.pop()
         ans[idx] = arr[i]
+    stack.append(i)
 
-print(' '.join(map(str, ans)))
+print(*ans)
