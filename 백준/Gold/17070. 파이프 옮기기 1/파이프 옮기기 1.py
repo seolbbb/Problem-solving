466cf2c48e3eb60eb076752f229ce85b3fd77c0c
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-01-25 20:58:43 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,40 +1,23 @@
-import sys
-input = input
-sys.setrecursionlimit(10**7)
-
-def dfs(r, c, d):
-    if memo[r][c][d] != -1:
-        return memo[r][c][d]
-    
-    if r == n - 1 and c == n - 1:
-        return 1
-    
-    ways = 0
-    
-    # 1) 가로 방향 이동이 가능한지 체크
-    if d == 0 or d == 1:
-        nr, nc = r, c + 1
-        if nc < n and board[nr][nc] == 0:
-            ways += dfs(nr, nc, 0)
-        
-    # 2) 세로 방향 이동이 가능한지 체크
-    if d == 2 or d == 1:
-        nr, nc = r + 1, c
-        if nr < n and board[nr][nc] == 0:
-            ways += dfs(nr, nc, 2)
-
-    # 3) 대각선 이동
-    nr, nc = r + 1, c + 1
-    if nr < n and nc < n:
-        if board[r][c+1] == 0 and board[r+1][c] == 0 and board[nr][nc] == 0:
-            ways += dfs(nr, nc, 1)
-    
-    memo[r][c][d] = ways
-    return ways
-
-
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
-memo = [[[-1, -1, -1] for _ in range(n)] for _ in range(n)]
+dp = [[[0, 0, 0] for _ in range(n)] for _ in range(n)]
 
-print(dfs(0, 1, 0))
+dp[0][1][0] = 1
+
+for r in range(n):
+    for c in range(n):
+        if board[r][c] == 1:
+            continue
+
+        if c-1 >= 0:
+            dp[r][c][0] += dp[r][c-1][0] + dp[r][c-1][1]
+        
+        if r-1 >= 0:
+            dp[r][c][2] += dp[r-1][c][1] + dp[r-1][c][2]
+        
+        if r-1 >= 0 and c-1 >= 0:
+            if board[r-1][c] == 0 and board[r][c-1] == 0:
+                dp[r][c][1] += dp[r-1][c-1][0] + dp[r-1][c-1][1] + dp[r-1][c-1][2]
+
+ans = dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2]
+print(ans)
