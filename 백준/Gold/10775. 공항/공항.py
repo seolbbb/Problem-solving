96a02ee25cf27d956d781742bdf79e6b507a2e28
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-01-26 13:47:44 +0900

    [Gold II] Title: 공항, Time: 180 ms, Memory: 41144 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
+def union(a, b):
+    pa = find(a)
+    pb = find(b)
+    if pa > pb:
+        parent[pa] = pb
+    else:
+        parent[pb] = pa
+
+def find(a):
+    if a != parent[a]:
+        parent[a] = find(parent[a])
+    return parent[a]
+
+g = int(input())
+p = int(input())
+parent = [i for i in range(g+1)]
+planes = [int(input()) for _ in range(p)]
+ans = 0
+
+for plane in planes:
+    if find(plane) == 0:
+        break
+    if plane == find(plane):
+        union(plane, plane-1)
+        ans += 1
+    else:
+        union(find(plane), find(plane)-1)
+        ans += 1
+
+print(ans)
