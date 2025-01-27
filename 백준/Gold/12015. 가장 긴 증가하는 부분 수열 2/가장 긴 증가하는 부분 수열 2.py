cbe13cd89e279911f0b48abb0a646074a632a908
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-01-27 18:56:46 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 756 ms, Memory: 134932 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,4 +1,5 @@
 import sys
+from bisect import bisect_left
 input = sys.stdin.readline
 
 n = int(input())
@@ -9,16 +10,7 @@ for num in arr:
     if num > lis[-1]:
         lis.append(num)
     else:
-        idx = 0
-        bl = 0
-        br = len(lis)-1
-
-        while bl <= br:
-            middle = (bl+br)//2
-            if lis[middle] >= num:
-                br = middle-1
-            else:
-                bl = middle+1
-        lis[bl] = num
+        idx = bisect_left(lis, num)
+        lis[idx] = num
 
 print(len(lis)-1)
