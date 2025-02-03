a52839908a527f56fcbaba9f9666f4ae89d57fb3
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-02-04 01:25:09 +0900

    [Gold IV] Title: 사이클 게임, Time: 1072 ms, Memory: 56936 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,37 @@
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
+def union(a, b):
+    pa = find(a)
+    pb = find(b)
+    if rank[pa] == rank[pb]:
+        parent[pa] = pb
+        rank[pb] += 1
+    elif rank[pa] > rank[pb]:
+        parent[pb] = pa
+    else:
+        parent[pa] = pb
+
+def find(a):
+    if parent[a] != a:
+        parent[a] = find(parent[a])
+    return parent[a]
+
+n, m = map(int, input().split())
+parent = [i for i in range(n)]
+rank = [1 for _ in range(n)]
+ans = 0
+
+for i in range(1, m+1):
+    a, b = map(int, input().split())
+
+    if ans > 0:
+        continue
+
+    if find(a) != find(b):
+        union(a, b)
+    else:
+        ans = i
+
+print(ans)
