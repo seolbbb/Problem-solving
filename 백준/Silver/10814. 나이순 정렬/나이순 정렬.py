9753e34be88249a168c5acfa60eb10e26ad6b8fb
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-06-23 15:42:29 +0900

    [Silver V] Title: 나이순 정렬, Time: 240 ms, Memory: 46020 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,14 +1,15 @@
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
+n = int(input())
 lst = []
 
-for i in range(n):
-    temp = list(sys.stdin.readline().split())
-    temp.append(i)
-    temp[0] = int(temp[0])
-    lst.append(temp)
-    
-lst.sort(key=lambda x: (x[0],x[2]))
-for i in range(n):
-    print(*lst[i][:-1])
+for _ in range(n):
+    a, b = map(str, input().rstrip().split())
+    a = int(a)
+    lst.append((a, b))
+
+lst.sort(key = lambda x:x[0])
+
+for age, name in lst:
+    print(age, name)
