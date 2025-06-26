4305bb84a740cf0cf9ae765701b5401b3b0dc48a
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-06-27 00:56:42 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,11 +1,13 @@
 t = int(input())
 
-lst = [[1,0]]
+# 0부터 40까지 저장
+dp = [(1, 0), (0, 1)]  # (fib(0) 호출 수, fib(1) 호출 수)
 
-for i in range(40):
-    lst.append([lst[i][1],(lst[i][0]+lst[i][1])])
+for i in range(2, 41):
+    a, b = dp[i-1]
+    c, d = dp[i-2]
+    dp.append((a + c, b + d))
 
-for i in range(t):
+for _ in range(t):
     n = int(input())
-
-    print(*lst[n])
+    print(dp[n][0], dp[n][1])
