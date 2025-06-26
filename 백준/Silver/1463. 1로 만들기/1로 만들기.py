8d190c16c13d594de72819cb44836060cc037a18
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-06-27 01:18:52 +0900

    [Silver III] Title: 1로 만들기, Time: 572 ms, Memory: 40660 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,18 +1,14 @@
+dp = [1000000 for _ in range(1000001)]
+dp[0] = 0
+dp[1] = 0
+
+for i in range(2, 1000001):
+    dp[i] = min(dp[i], dp[i-1] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+
 n = int(input())
 
-dp = [0,1,1]
-
-for i in range(4,n+1):
-    m = float('inf')
-
-    if i % 3 == 0:
-        if (1 + dp[(i//3)-1]) < m:
-            m = (1 + dp[(i//3)-1])
-    if i % 2 == 0:
-        if (1 + dp[(i//2)-1]) < m:
-            m = (1 + dp[(i//2)-1])
-    if (1 + dp[i-2]) < m:
-        m = (1 + dp[i-2])
-    dp.append(m)
-
-print(dp[n-1])
+print(dp[n])
