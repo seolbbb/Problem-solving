f95ceb7838b4d74e1e808178be4c7f1a81d537df
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-06-28 21:53:15 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,21 +1,16 @@
 n = int(input())
-score = [0 for _ in range(n+1)]
+score = [0] + [int(input()) for _ in range(n)]
 dp = [0 for _ in range(n+1)]
-
-for i in range(1,n+1):
-    score[i] = (int(input()))
 
 if n == 1:
     print(score[1])
-    exit(0)
 elif n == 2:
-    print(score[1] + score[2])
-    exit(0)
+    print(score[1]+score[2])
+else:
+    dp[1] = score[1]
+    dp[2] = score[1] + score[2]
 
-dp[1] = score[1]
-dp[2] = score[1] + score[2]
+    for i in range(3, n+1):
+        dp[i] = max(dp[i-2]+score[i], dp[i-3]+score[i-1]+score[i])
 
-for i in range(3,n+1):
-    dp[i] = max(dp[i-3] + score[i-1] + score[i], dp[i-2] + score[i])
-
-print(dp[n])
+    print(dp[n])
