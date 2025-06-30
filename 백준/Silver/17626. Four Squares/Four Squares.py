02f6d0884009bc885e9def6bf81bd96b5002181c
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-06-30 18:40:56 +0900

    [Silver III] Title: Four Squares, Time: 128 ms, Memory: 110060 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,20 +1,13 @@
-import math
+dp = [4 for _ in range(50001)]
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
 
-memo = {}
+for i in range(4, 50001):
+    for j in range(int(i**0.5), 0, -1):
+        dp[i] = min(dp[i], dp[i-j**2]+1)
 
 n = int(input())
 
-for i in range(1,n+1):
-    root = math.isqrt(i)
-    minimum = 4
-    if i == root**2:
-        memo[i] = 1
-    else:
-        for j in range(1,root+1):
-            cnt = 1
-            cnt += memo[i-j**2]
-            if cnt < minimum:
-                minimum = cnt
-        memo[i] = minimum
-
-print(memo[n])
+print(dp[n])
