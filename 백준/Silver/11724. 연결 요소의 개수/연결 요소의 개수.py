b1d1d6b36b98c11928715901968f126f3aeef20c
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-07-05 15:00:48 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 400 ms, Memory: 176992 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,31 +1,30 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
-adj = [[] for _ in range(1005)]
-vis = [0 for _ in range(1005)]
-stack = deque()
-num = 0
-
-n, m = map(int,input().split())
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visit = [0 for _ in range(n+1)]
+cnt = 0
 
 for _ in range(m):
-    u, v = map(int,input().split())
-    adj[u].append(v)
-    adj[v].append(u)
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-for i in range(1,n+1):
-    if vis[i]:
+for i in range(1, n+1):
+    if visit[i]:
         continue
-    num += 1
-    stack.append(i)
-    vis[i] = 1
-    while stack:
-        cur = stack.pop()
-        for j in adj[cur]:
-            if vis[j]:
-                continue
-            stack.append(j)
-            vis[j] = 1
 
-print(num)
+    queue = deque([i])
+    visit[i] = 1
+    cnt += 1
+
+    while queue:
+        cur = queue.popleft()
+
+        for nxt in graph[cur]:
+            if visit[nxt]:
+                continue
+            queue.append(nxt)
+            visit[nxt] = 1
+
+print(cnt)
