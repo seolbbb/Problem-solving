e858458df712dc102d43de53bcbb545beb62cc8a
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-07-05 15:11:59 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 532 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,30 +1,29 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a > b:
+        parent[a] = b
+    elif a < b:
+        parent[b] = a
+
+def find(a):
+    if parent[a] != a:
+        parent[a] = find(parent[a])
+    return parent[a]
 
 n, m = map(int, input().split())
-graph = [[] for _ in range(n+1)]
-visit = [0 for _ in range(n+1)]
-cnt = 0
+parent = [i for i in range(n+1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    union(a, b)
 
 for i in range(1, n+1):
-    if visit[i]:
-        continue
+    find(i)
 
-    queue = deque([i])
-    visit[i] = 1
-    cnt += 1
-
-    while queue:
-        cur = queue.popleft()
-
-        for nxt in graph[cur]:
-            if visit[nxt]:
-                continue
-            queue.append(nxt)
-            visit[nxt] = 1
-
-print(cnt)
+print(len(set(parent))-1)
