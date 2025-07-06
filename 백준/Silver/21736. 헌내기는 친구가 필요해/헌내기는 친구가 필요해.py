1bdca726f5ac724acdc2903d116ad47274b1175b
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-07-06 14:59:14 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 776 ms, Memory: 41700 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -2,35 +2,34 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-n, m = map(int,input().split())
+n, m = map(int, input().split())
+dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 board = [list(input().rstrip()) for _ in range(n)]
 visit = [[0 for _ in range(m)] for _ in range(n)]
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
-queue = deque()
-cnt = 0
+queue = deque([])
+ans = 0
 
 for i in range(n):
     for j in range(m):
         if board[i][j] == 'I':
-            queue.append((i,j))
+            queue.append((i, j))
             visit[i][j] = 1
 
 while queue:
     x, y = queue.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+
+    for dx, dy in dir:
+        nx = x + dx
+        ny = y + dy
+
         if nx < 0 or nx >= n or ny < 0 or ny >= m:
             continue
         if visit[nx][ny] == 1 or board[nx][ny] == 'X':
             continue
         if board[nx][ny] == 'P':
-            cnt += 1
-        queue.append((nx,ny))
+            ans += 1
+        
+        queue.append((nx, ny))
         visit[nx][ny] = 1
 
-if cnt == 0:
-    print('TT')
-else:
-    print(cnt)
+print(ans if ans else 'TT')
