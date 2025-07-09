334b634a330fd8715a71628666c954174503d5c8
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-07-09 15:01:33 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 38176 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,22 +1,20 @@
 from collections import deque
-n, k = map(int,input().split())
-queue = deque()
-dist = []
-for i in range(100001):
-    dist.append(-1)
-queue.append(n)
-dist[n] = 0
+
+n, k = map(int, input().split())
+
+time = [-1 for _ in range(100001)]
+queue = deque([n])
+time[n] = 0
 
 while queue:
-    x = queue.popleft()
-    dx = [-1,1,x]
-    if x == k:
-        print(dist[x])
-    for i in range(3):
-        nx = x + dx[i]
-        if nx < 0 or nx > 100000:
+    cur = queue.popleft()
+    move = (cur - 1, cur + 1, cur * 2)
+
+    for nxt in move:
+        if nxt < 0 or nxt > 100000:
             continue
-        if dist[nx] >= 0:
-            continue
-        queue.append(nx)
-        dist[nx] = dist[x] + 1
+        if time[nxt] == -1:
+            time[nxt] = time[cur] + 1
+            queue.append(nxt)
+
+print(time[k])
