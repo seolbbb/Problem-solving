e0cb5d5ca30b020dc4e2d335be51664e9e1a1173
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-07-10 15:49:17 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 692 ms, Memory: 41920 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -2,39 +2,33 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-n, m = map(int,input().split())
-board = [[] for _ in range(n)]
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+queue = deque([])
 dist = [[-1 for _ in range(m)] for _ in range(n)]
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
-queue = deque()
-
-for i in range(n):
-    board[i] = list(map(int,input().split()))
+dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 for i in range(n):
     for j in range(m):
         if board[i][j] == 2:
-            queue.append((i,j))
+            queue.append((i, j))
+            dist[i][j] = 0
+        elif board[i][j] == 0:
             dist[i][j] = 0
 
 while queue:
-    x,y = queue.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+    x, y = queue.popleft()
+
+    for dx, dy in dir:
+        nx = x + dx
+        ny = y + dy
+
         if nx < 0 or nx >= n or ny < 0 or ny >= m:
             continue
         if board[nx][ny] == 0 or dist[nx][ny] >= 0:
             continue
-        queue.append((nx,ny))
+        queue.append((nx, ny))
         dist[nx][ny] = dist[x][y] + 1
-
-
-for i in range(n):
-    for j in range(m):
-        if dist[i][j] == -1 and board[i][j] == 0:
-            dist[i][j] = 0
 
 for row in dist:
     print(*row)
