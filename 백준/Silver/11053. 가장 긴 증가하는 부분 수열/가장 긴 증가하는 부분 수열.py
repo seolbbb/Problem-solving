28ff457327f1e3afa5e8144782a6737ddcb9d290
설성범 <85074180+tjftjdqja@users.noreply.github.com>
2025-07-11 17:27:33 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,23 +1,19 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-lst = list(map(int,input().split()))
-dp = [0 for _ in range(1001)]
+arr = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(n+1)]
 
 dp[1] = 1
 
-for i in range(2,n+1):
-    tmp = []
+for i in range(2, n+1):
+    lst = []
 
-    for j in range(i-1):
-        if lst[i-1] > lst[j]:
-            tmp.append((dp[j+1]))
+    for j in range(i):
+        if arr[i] > arr[j]:
+            lst.append(dp[j])
 
-    if len(tmp) == 0:
-        dp[i] = 1
+    if lst:
+        dp[i] = max(lst) + 1
     else:
-        pre = max(tmp)
-        dp[i] = pre + 1
+        dp[i] = 1
 
 print(max(dp))
