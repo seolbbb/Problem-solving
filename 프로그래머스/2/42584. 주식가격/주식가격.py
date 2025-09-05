ad7dae349f06719044abb0fd57f639007dc8edf5
설성범 <85074180+seolbbb@users.noreply.github.com>
2025-09-05 09:49:18 +0900

    [level 2] Title: 주식가격, Time: 24.44 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,16 +1,30 @@
+# 처음에는 stack의 뒤에서부터 탐색하는 걸 생각함
+# 뒤에서부터 탐색하면서 시간을 1초씩 더하고, 현재 나온 숫자보다 작은 수가 나온 시점을 기록해서 빼 줌
+# 예시)
+# [1, 2, 3, 2, 1, 3]
+
+# 3 : 0초
+# 1 : 1초
+# 2 : 2초 - 1초(1초에 2보다 작은 수 나옴)
+# 3 : 3초 - 2초(2초에 3보다 작은 수 나옴)
+# 2 : 4초 - 1초(1초에 2보다 작은 수 나옴)
+# 1 : 5초
+
+# 근데 현재 가격보다 작은 수가 나온 시점을 탐색하는 과정에서 시간 복잡도 O(n)
+# stack 탐색 loop 안에 들어가니까 시간 복잡도 O(n^2)이 나와서 이 방법은 포기함
+
+
 def solution(prices):
     n = len(prices)
     ans = [0] * n
-    stack = []  # 아직 하락이 확정되지 않은 인덱스들
+    stack = []
 
     for i, p in enumerate(prices):
-        # 현재 p가 더 작아졌다면(하락), 그동안 쌓여있던 인덱스들의 종료를 확정
-        while stack and prices[stack[-1]] > p:  # '>' (같은 값은 하락 아님)
+        while stack and prices[stack[-1]] > p:
             j = stack.pop()
             ans[j] = i - j
         stack.append(i)
 
-    # 끝까지 하락 안 온 것들 처리
     while stack:
         j = stack.pop()
         ans[j] = (n - 1) - j
