88c66ed33624c3e76e16348721dfa5f2ac98b315
설성범 <85074180+seolbbb@users.noreply.github.com>
2025-09-11 11:01:25 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 328.33 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -2,16 +2,14 @@ cnt_zero = 0
 cnt_one = 0
 
 def solution(arr):
-
-    
     def quad(r, c, size):
         global cnt_zero
         global cnt_one
-        cur = arr[r][c]
+        cur = arr[r][c]     # 처음 값 기록
         
-        for i in range(r, r + size):
+        for i in range(r, r + size):    
             for j in range(c, c + size):
-                if arr[i][j] != cur:
+                if arr[i][j] != cur:    # 구역 탐색하다가 처음 값과 다른 값 있으면, 다시 4등분으로 나누어서 재귀
                     for k in range(2):
                         for l in range(2):
                             quad(r + k * (size//2), c + l * (size//2), size//2)
